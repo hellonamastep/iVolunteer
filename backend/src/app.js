e4196d30b01702c43f1ac46d74ed2df95faca6c9
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2025-10-15 19:00:01 +0530

    Change cors url 1.2

--- backend/src/app.js
+++ backend/src/app.js
@@ -36,8 +36,8 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    // origin: "http://localhost:3000", // frontend URL
-    origin: "https://namastep-irod.onrender.com",
+     origin: "http://localhost:3000", // frontend URL
+    origin: "https://namastep.vercel.app/",
     credentials: true,
   })
 );
