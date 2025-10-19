c886f2af953105c0be3d77bd9f59db88f6346144
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2025-10-19 17:29:48 +0530

    fixed api.ts  baseurl

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiHost = (process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000").replace(/\/$/, "");
 const api = axios.create({
-  baseURL: `${apiHost}/api`,
+  baseURL: `${apiHost}`,
   withCredentials: true,
   headers: {
     "Cache-Control": "no-cache",
